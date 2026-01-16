94374dc25feaa1a3866f9ccd85db5330854fb7d1
madhav3289 <madhavguptadavfps1234@gmail.com>
2026-01-16 23:33:24 +0530

    "Added Factory Machines Problem Solution"

---
+++ sorting_and_searching/Factory_Machines.java
@@ -0,0 +1,44 @@
+package cses_problemset.sorting_and_searching;
+
+import java.util.*;
+
+public class Factory_Machines {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        long t=in.nextLong();
+        long [] nums=new long[n];
+        for(int i=0;i<n;i++){
+            nums[i]=in.nextLong();
+        }
+        Arrays.sort(nums);
+        System.out.println(minTime(nums,t,n));
+        in.close();
+    }
+    public static long minTime(long [] nums,long t,int n){
+        long ans=-1;
+        long lo=1;
+        long hi=nums[n-1]*t;
+        while(lo<=hi){
+            long mid=lo+(hi-lo)/2;
+            if(isPossible(nums,mid,t)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static boolean isPossible(long [] nums,long mid,long t){
+        long count=0;
+        for(int i=0;i<nums.length;i++){
+            count+=mid/nums[i];
+            if(count>=t){
+                return true;
+            }
+        }
+        return count>=t;
+    }
+}
