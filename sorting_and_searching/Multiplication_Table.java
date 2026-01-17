31b278eab2590b56ddf0397373887f9efcfd45f7
madhav3289 <madhavguptadavfps1234@gmail.com>
2026-01-17 12:17:41 +0530

    "Added Multiplication Table Problem Solution"

---
+++ sorting_and_searching/Multiplication_Table.java
@@ -0,0 +1,36 @@
+package cses_problemset.sorting_and_searching;
+
+import java.util.*;
+
+public class Multiplication_Table {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        long n=in.nextLong();
+        System.out.println(median(n));
+        in.close();
+    }
+    public static long median(long n){
+        long lo=1;
+        long hi=n*n;
+        long ans=hi;
+        while(lo<=hi){
+            long mid=lo+(hi-lo)/2;
+            if(isPossible(mid,n)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static boolean isPossible(long mid,long n){
+        long count=0;
+        long req=((long)n*n+1)/2;
+        for(int i=1;i<=n;i++){
+            count+=Math.min(n,mid/i);
+        }
+        return count>=req;
+    }
+}
