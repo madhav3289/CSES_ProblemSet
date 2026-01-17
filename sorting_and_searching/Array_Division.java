6917a8a7a5b87500b57bfbb239c989e72b6277a6
madhav3289 <madhavguptadavfps1234@gmail.com>
2026-01-17 19:52:56 +0530

    "Added solution of Array Division problem"

---
+++ sorting_and_searching/Array_Division.java
@@ -0,0 +1,55 @@
+package cses_problemset.sorting_and_searching;
+
+import java.util.*;
+
+public class Array_Division {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        long k=in.nextLong();
+        long [] nums=new long[n];
+        for(int i=0;i<n;i++){
+            nums[i]=in.nextLong();
+        }
+        System.out.println(minSubarraySum(nums,k,n));
+        in.close();
+    }
+    public static long minSubarraySum(long [] nums,long k,int n){
+        long sum=0;
+        long max=0;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,nums[i]);
+            sum+=nums[i];
+        }
+        long lo=max;
+        long hi=sum;
+        long ans=sum;
+        while(lo<=hi){
+            long mid=lo+(hi-lo)/2;
+            if(isPossible(nums,k,n,mid)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static boolean isPossible(long [] nums,long k,int n,long mid){
+        long cSum=0;
+        long count=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>mid) return false;
+            if(cSum+nums[i]>mid){
+                cSum=nums[i];
+                count++;
+            }
+            else{
+                cSum+=nums[i];
+            }
+        }
+        return count<=k;
+    }
+    
+}
